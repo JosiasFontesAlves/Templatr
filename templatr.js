7cb59e1245f25d35d5e4a965f2998476d6668ec8
Josias <jkodja@gmail.com>
2021-10-13 20:20:41 -0300

    Update templatr.js

--- templatr.js
+++ templatr.js
@@ -1,16 +1,12 @@
 function templatr() {
-    const { body } = document, res = [];
-
-    [...arguments].forEach(elem => res.push(document.createElement(typeof elem === 'string' ? elem : Object.keys(elem))));
-
-    res.forEach((el, i) => {
-        if (typeof arguments[i] === 'object') {
-            for (let tag in arguments[i]) {
-                for (let atr in arguments[i][tag])
-                    el[atr] = arguments[i][tag][atr];
-            }
+    [...arguments].forEach(elem => {
+        const el = document.createElement(typeof elem === 'string' ? elem : Object.keys(elem)[0]);
+        if (typeof elem === 'object') {
+            for (let tag in elem)
+                Object.entries(elem[tag]).forEach(([atr, val]) => el.setAttribute(atr, val));
         }
+        document.body.appendChild(el);
     });
-
-    body.append(...res);
 }
+
+templatr('header', { div: { id: 'root' } });
